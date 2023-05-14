32c98b935978549aa341174fd3e8a284c75952d3
yongjun18 <yongjun18976@gmail.com>
2023-05-14 13:18:16 +0900

    new project, git 세팅

---
+++ MarioJump/MarioJumpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarioJumpApp.swift
+//  MarioJump
+//
+//  Created by yongjun18 on 2023/05/14.
+//
+
+import SwiftUI
+
+@main
+struct MarioJumpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
